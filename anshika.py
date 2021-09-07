d144aca0a5c8f9477ac544ca91f196f0cc641a86
Anshikaverma24 <90278284+Anshikaverma24@users.noreply.github.com>
2021-09-07 23:33:23 +0530

    Add files via upload

---
+++ anshika.py
@@ -0,0 +1,20 @@
+number=2
+print(number)
+number=2.3
+print(number)
+number=4+2j
+print(number)
+a=int(input("enter your age :"))
+b=int(input("enter the number :"))
+sum=a+b
+print("sum =",sum)
+
+a=str(input("enter your name :"))
+b= str(input("enter last name :"))
+sum=a+b
+print(sum)
+
+a=int(input("enter your age :"))
+b=str(input("enter your name :"))
+add= a+b
+print(add)
